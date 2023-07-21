bed1e0c6b6bd7462c32962bb2c53533ce893d155
Neyan Montes <neyanmontes611@gmail.com>
2023-07-21 18:52:35 -0500

    Default React (Redesign)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <nav className='navbar'>
-          <div className='nav-left'>
-            <a href='#'><img src={logo} alt='Shopify Plus Logo'></img></a>
-
-            <ul>
-              <li><a href='#'>Vende</a></li>
-              <li><a href='#'>Gestiona</a></li>
-              <li><a href='#'>Integra</a></li>
-              <li><a href='#'>Plataforma</a></li>
-              <li><a href='#'>Precios</a></li>
-            </ul>
-          </div>
-
-          <ul className='nav-right'>
-            <li><a href='#'>Contactar con el equipo de ventas</a></li>
-          </ul>
-        </nav>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
