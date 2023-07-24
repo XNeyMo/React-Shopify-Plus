9cf4ad9268ebcb8692b00a122456562a56cce482
Neyan Montes <neyanmontes611@gmail.com>
2023-07-24 03:12:02 -0500

    Description 2 Finished...

--- src/components/Description2.jsx
+++ src/components/Description2.jsx
@@ -1,47 +1,41 @@
 export const Description2 = () => {
     return (
-        <section className="px-[5.6rem] bg-white">
+        <section className="px-[5.6rem] bg-white py-[7.2rem]">
             <div className="flex">
                 <div className="w-6/12">
-                    <p>Diseña para convertir</p>
-                    <h1>Convierte hasta un 91% más desde el primer día</h1>
-                    <p>Más información</p>
+                    <p className="uppercase text-[#1a41c0] font-bold text-[1.32rem] tracking-tighter">Diseñada para convertir</p>
+                    <h1 className="font-bold text-[3.6rem] pr-[8rem] leading-[4rem] mb-[1rem]">Convierte hasta un 91% más desde el primer día</h1>
+                    <p className="text-[1.32rem]">Más información</p>
                 </div>
 
-                <div className="flex w-6/12">
-                    <div>
-                        <div className="circle"></div>
-                        <div className="circle"></div>
-                        <div className="circle"></div>
-                    </div>
-
-                    <div>
-                        <p>Atrae a compradores que estén buscando productos como los tuyos con audiencias segmentadas por datos en marketplaces sociales populares.</p>
-                        <p>Siempre realiza las ventas con Shop Pay, el proceso de pago de mejor conversión en internet.</p>
-                        <p>Retén a los clientes con programas de fidelización y un toque personal desde la página de inicio hasta la bandeja de entrada.</p>
-                    </div>
+                <div className="w-6/12">
+                    <ul className="list-image-checkmark text-[1.32rem]">
+                        <li>Atrae a compradores que estén buscando productos como los tuyos con audiencias segmentadas por datos en marketplaces sociales populares.</li>
+                        <li>Siempre realiza las ventas con Shop Pay, el proceso de pago de mejor conversión en internet.</li>
+                        <li>Retén a los clientes con programas de fidelización y un toque personal desde la página de inicio hasta la bandeja de entrada.</li>
+                    </ul>
                 </div>
             </div>
 
-            <div className="flex">
-                <div className="w-3/12">
-                    <h1>91%</h1>
-                    <p>más de conversión en dispositivos móviles con Shop Pay que los procesos de pago habituales</p>
+            <div className="grid grid-cols-4 gap-[1rem] pt-[5rem]">
+                <div>
+                    <h1 className="mt-[-12px] text-[4.5rem] font-bold text-[#1a41c0]">91%</h1>
+                    <p className="mt-[-12px] text-[1.15rem]">más de conversión en dispositivos móviles con Shop Pay que los procesos de pago habituales</p>
                 </div>
 
-                <div className="w-3/12">
-                    <h1>4 veces</h1>
-                    <p>más rapidez en los pagos con Shop Pay</p>
+                <div>
+                    <h1 className="mt-[-12px] text-[4.5rem] font-bold text-[#1a41c0]">4 veces</h1>
+                    <p className="mt-[-12px] text-[1.15rem]">más rapidez en los pagos con Shop Pay</p>
                 </div>
 
-                <div className="w-3/12">
-                    <h1>6,6 veces</h1>
-                    <p>más de retorno de la inversión publicitaria con Shopify Audiences</p>
+                <div>
+                    <h1 className="text-[4.5rem] font-bold text-[#1a41c0] leading-[5rem]">6,6 veces</h1>
+                    <p className="text-[1.15rem]">más de retorno de la inversión publicitaria con Shopify Audiences</p>
                 </div>
 
-                <div className="w-3/12">
-                    <h1>100M+</h1>
-                    <p>de compradores de todo el mundo usan Shop Pay</p>
+                <div>
+                    <h1 className="mt-[-12px] text-[4.5rem] font-bold text-[#1a41c0]">100M+</h1>
+                    <p className="mt-[-12px] text-[1.15rem]">de compradores de todo el mundo usan Shop Pay</p>
                 </div>
             </div>
         </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+
   theme: {
-    extend: {},
+    extend: {
+      listStyleImage: {
+        checkmark: 'url("/src/Img/check.png")',
+      },
+    },
   },
+
   plugins: [],
 }
 
