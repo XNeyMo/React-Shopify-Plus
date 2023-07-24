42d2ec185afd9256183fba4d77a9d5f647778b7c
Neyan Montes <neyanmontes611@gmail.com>
2023-07-24 03:27:04 -0500

    Message 1 Finished

--- src/components/Message1.jsx
+++ src/components/Message1.jsx
@@ -1,13 +1,13 @@
 export const Message1 = () => {
     return (
-        <section className="px-[5.6rem] text-white">
+        <section className="px-[5.6rem] text-white py-[7.2rem]">
             <div className="flex">
-                <h1 className="w-6/12">"Shopify Plus es rapidísimo. Es nuestro portal para clientes en cinco países y nos permite diseñar promociones personalizadas que, por cierto, han resultado muy efectivas."</h1>
+                <h1 className="w-6/12 text-[3.2rem] leading-[3.25rem]">"Shopify Plus es rapidísimo. Es nuestro portal para clientes en cinco países y nos permite diseñar promociones personalizadas que, por cierto, han resultado muy efectivas."</h1>
 
-                <img className="w-6/12" src="https://cdn.shopify.com/b/shopify-brochure2-assets/b15596803f7b326f0e5025dc0c58d25f.svg?crop=none" alt="Shopify Pure" />
+                <img className="w-4/12 ml-[calc((100%-11rem)/12)]" src="https://cdn.shopify.com/b/shopify-brochure2-assets/b15596803f7b326f0e5025dc0c58d25f.svg?crop=none" alt="Shopify Pure" />
             </div>
 
-            <p><span>Ric Kostick</span> — Cofundador, 100% Pure</p>
+            <p className="mt-[2.5rem]"><span className="font-bold">Ric Kostick</span> — Cofundador, 100% Pure</p>
         </section>
     )
 }
