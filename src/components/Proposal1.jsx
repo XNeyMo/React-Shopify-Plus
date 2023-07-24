ef8d13c3702acd01a52c5c7c019701fe09e8dde2
Neyan Montes <neyanmontes611@gmail.com>
2023-07-24 03:38:50 -0500

    Proposal 1 Finished

--- src/components/Proposal1.jsx
+++ src/components/Proposal1.jsx
@@ -1,8 +1,8 @@
 export const Proposal1 = () => {
     return (
-        <section className="px-[5.6rem] h-screen bg-[#f3f4f6]">
-            <h1>Tus clientes no se van a quedar quietos. Tú tampoco puedes hacerlo.</h1>
-            <button>Contacta con el departamento de ventas</button>
+        <section className="px-[5.6rem] h-[calc(100vh-73px)] bg-[#f3f4f6] py-[7.2rem]">
+            <h1 className="font-bold text-[3.6rem] mr-[25rem] leading-[4rem] tracking-normal">Tus clientes no se van a quedar quietos. Tú tampoco puedes hacerlo.</h1>
+            <button className="mt-[5rem] bg-[#1a41c0] py-[.9rem] px-[1.8rem] text-white font-bold text-[18px] rounded-[40px] tracking-normal">Contacta con el departamento de ventas</button>
         </section>
     )
 }
