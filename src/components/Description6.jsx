b305f43eb3297421292aec564e80d14ccb56c483
Neyan Montes <neyanmontes611@gmail.com>
2023-07-25 23:35:18 -0500

    Description 6 Finished...

--- src/components/Description6.jsx
+++ src/components/Description6.jsx
@@ -1,33 +1,26 @@
 export const Description6 = () => {
     return (
-        <section className="px-[5.6rem] bg-white">
-            <p>Comprometidos con la atencion al cliente</p>
-            <h1>Cuenta con toda la asistencia que necesites</h1>
+        <section className="px-[5.6rem] bg-white py-[7.2rem]">
+            <p className="uppercase text-[#1a41c0] font-bold text-[1.32rem]">Comprometidos con la atencion al cliente</p>
+            <h1 className="font-bold text-[3.6rem] pr-[8rem] leading-[4rem] mb-[1rem] tracking-normal">Cuenta con toda la asistencia que necesites</h1>
 
             <div className="flex">
-                <div className="w-6/12">
-                    <div>
-                        <div className="circle"></div>
-                        <div className="circle"></div>
-                        <div className="circle"></div>
-                        <div className="circle"></div>
-                    </div>
-
-                    <div>
-                        <p>Atención al cliente técnica prioritaria 24/7 por teléfono, email o chat en vivo</p>
-                        <p>Conocimiento de la plataforma e información útil sobre el sector mediante el Programa de Éxito de los Emprendedores (Merchant Success)</p>
-                        <p>Migración personalizada, lanzamiento y atención al cliente para la gestión de cuentas sin coste adicional</p>
-                        <p>Personalización de la plataforma con más de 8.000 apps y cientos de partners de Shopify Plus y aplicaciones certificadas</p>
-                    </div>
+                <div className="w-6/12 pr-[15.5rem]">
+                    <ul className="list-image-checkmark text-[1.32rem]">
+                        <li>Atención al cliente técnica prioritaria 24/7 por teléfono, email o chat en vivo</li>
+                        <li>Conocimiento de la plataforma e información útil sobre el sector mediante el Programa de Éxito de los Emprendedores (Merchant Success)</li>
+                        <li>Migración personalizada, lanzamiento y atención al cliente para la gestión de cuentas sin coste adicional</li>
+                        <li>Personalización de la plataforma con más de 8.000 apps y cientos de partners de Shopify Plus y aplicaciones certificadas</li>
+                    </ul>
                 </div>
 
-                <img className="w-6/12" src="https://cdn.shopify.com/shopifycloud/plus_website/assets/pages/home/launch-engineers-large-401fe5a3e8d4ab934811a90871146cd7758efa167896901b937fcae8db70e51e.jpg?crop=none" alt="" />
+                <img className="w-6/12 h-[454.11px]" src="https://cdn.shopify.com/shopifycloud/plus_website/assets/pages/home/launch-engineers-large-401fe5a3e8d4ab934811a90871146cd7758efa167896901b937fcae8db70e51e.jpg?crop=none" alt="" />
             </div>
 
-            <h1>Our customers say it better</h1>
-            <img src="https://cdn.shopify.com/b/shopify-brochure2-assets/3ae52b02e6420cfb957397fe42568cc9.png?originalWidth=290&originalHeight=97&crop=none" alt="" />
+            <h1 className="font-bold text-[2.9rem] pr-[8rem] leading-[4rem] mb-[1rem] tracking-normal mt-[3.5rem]">Our customers say it better</h1>
+            <img className="mb-[1.5rem]" src="https://cdn.shopify.com/b/shopify-brochure2-assets/3ae52b02e6420cfb957397fe42568cc9.png?originalWidth=290&originalHeight=97&crop=none" alt="" />
 
-            <a href="">Read real, unbiased customer reviews on G2 </a>
+            <a className="text-[#1a41c0] font-bold text-[1.1rem] underline" href="">Read real, unbiased customer reviews on G2 </a>
         </section>
     )
 }
