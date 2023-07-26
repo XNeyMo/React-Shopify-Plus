5d580aa0e311ff9cb934733f56800cbb1d01d228
Neyan Montes <neyanmontes611@gmail.com>
2023-07-25 21:59:07 -0500

    Credits Finished

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -1,11 +1,11 @@
 export const Credits = () => {
     return (
-        <section className="flex h-screen text-white px-[5.6rem]">
+        <section className="flex h-[calc(screen-73px)] text-white px-[5.6rem] py-[7.2rem]">
             <img className="w-6/12" src="https://cdn.shopify.com/b/shopify-brochure2-assets/d02d89645e9eb58a1f6bc1c45b813023.svg?crop=none" alt="Fast Company Logo" />
 
-            <div className="w-6/12">
-                <h1>Fast Company nos nombró la tercera empresa más innovadora del mundo en 2021</h1>
-                <button>Read the story</button>
+            <div className="w-6/12 pl-[7rem]">
+                <h1 className="text-[3.2rem] font-bold leading-[3.5rem]">Fast Company nos nombró la tercera empresa más innovadora del mundo en 2021</h1>
+                <button className="mt-[1.5rem] border-2 border-[#d0f224] text-[#d0f224] rounded-[50px] py-[0.8rem] px-[1.4rem] font-bold tracking-normal text-[1.1rem] hover:border-[3px]">Read the story</button>
             </div>
         </section>
     )
