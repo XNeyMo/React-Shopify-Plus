3b48a261336354cca0f161ee0846bae23570468f
Neyan Montes <neyanmontes611@gmail.com>
2023-07-25 22:13:46 -0500

    Message 3 Finished

--- src/components/Message3.jsx
+++ src/components/Message3.jsx
@@ -1,13 +1,13 @@
 export const Message3 = () => {
     return (
-        <section className="px-[5.6rem] bg-white">
+        <section className="px-[5.6rem] bg-white py-[7.2rem]">
             <div className="flex">
-                <h1>"Hemos crecido un 3.800% a lo largo de tres años. Ahora somos una marca omnicanal internacional. Como Shopify Plus se ha ido adaptando a nosotros durante todo el proceso, no tuvimos que convertirnos en una empresa de tecnología."</h1>
+                <h1 className="w-6/12 text-[3.2rem] leading-[3.25rem] whitespace-pre-wrap pr-[1rem]">"Hemos crecido un 3.800% a lo largo de tres años. Ahora somos una marca omnicanal internacional. Como Shopify Plus se ha ido adaptando a nosotros durante todo el proceso, no tuvimos que convertirnos en una empresa de tecnología."</h1>
 
-                <img src="https://cdn.shopify.com/b/shopify-brochure2-assets/4f29178313b30455fb8af2f9e6d23cbe.svg?crop=none" alt="knix logo" />
+                <img className="w-4/12 ml-[9%] mt-[-9%]" src="https://cdn.shopify.com/b/shopify-brochure2-assets/4f29178313b30455fb8af2f9e6d23cbe.svg?crop=none" alt="knix logo" />
             </div>
 
-            <p><span>Joanna Griffiths</span> — Fundadora y directora ejecutiva, Knix.</p>
+            <p className="mt-[2.5rem]"><span className="font-bold">Joanna Griffiths</span> — Fundadora y directora ejecutiva, Knix.</p>
         </section>
     )
 }
