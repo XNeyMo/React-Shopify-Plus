a0c678fb9be645ed7ff2496f843dd94144f28ec4
Neyan Montes <neyanmontes611@gmail.com>
2023-07-25 22:03:20 -0500

    Proposal 1 Modified and Proposal 2 Finished

--- src/components/Proposal1.jsx
+++ src/components/Proposal1.jsx
@@ -2,7 +2,7 @@ export const Proposal1 = () => {
     return (
         <section className="px-[5.6rem] h-[calc(100vh-73px)] bg-[#f3f4f6] py-[7.2rem]">
             <h1 className="font-bold text-[3.6rem] mr-[25rem] leading-[4rem] tracking-normal">Tus clientes no se van a quedar quietos. Tú tampoco puedes hacerlo.</h1>
-            <button className="mt-[5rem] bg-[#1a41c0] py-[.9rem] px-[1.8rem] text-white font-bold text-[18px] rounded-[40px] tracking-normal">Contacta con el departamento de ventas</button>
+            <button className="mt-[5rem] bg-[#1a41c0] py-[.9rem] px-[1.8rem] text-white font-bold text-[18px] rounded-[40px] tracking-normal hover:bg-[#485cdc]">Contacta con el departamento de ventas</button>
         </section>
     )
 }
--- src/components/Proposal2.jsx
+++ src/components/Proposal2.jsx
@@ -1,8 +1,8 @@
 export const Proposal2 = () => {
     return (
-        <section className="px-[5.6rem] h-screen bg-[#f3f4f6]">
-            <h1>Consigue la solución de pago con el mejor índice de conversión del mundo, con pago con un solo clic totalmente personalizable, solo en Shopify Plus.</h1>
-            <button>Contacta con el departamento de ventas</button>
+        <section className="px-[5.6rem] bg-[#f3f4f6] py-[7.2rem]">
+            <h1 className="font-bold text-[3.6rem] mr-[25rem] leading-[4rem] tracking-normal">Consigue la solución de pago con el mejor índice de conversión del mundo, con pago con un solo clic totalmente personalizable, solo en Shopify Plus.</h1>
+            <button className="mt-[5rem] bg-[#1a41c0] py-[.9rem] px-[1.8rem] text-white font-bold text-[18px] rounded-[40px] tracking-normal hover:bg-[#485cdc]">Contacta con el departamento de ventas</button>
         </section>
     )
 }
