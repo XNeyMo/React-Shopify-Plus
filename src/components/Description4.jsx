936b63fd077cb9ebc1ea7ca6a66b23e13195aae3
Neyan Montes <neyanmontes611@gmail.com>
2023-07-25 21:09:12 -0500

    Description 4 Finished...

--- src/components/Description4.jsx
+++ src/components/Description4.jsx
@@ -1,26 +1,19 @@
 export const Description4 = () => {
     return (
-        <section className="px-[5.6rem] bg-white flex">
+        <section className="px-[5.6rem] bg-white flex py-[7.2rem]">
             <div className="w-6/12">
-                <p>Diseñada para ser veloz</p>
-                <h1>Actúa rápido e innova aún más rápido.</h1>
-                <p>Llega al mercado antes que nadie usando nuevas funciones a medida que salen y responde a las tendencias de consumo cambiantes en tiempo real, con el apoyo de todo un ecosistema de partners.</p>
+                <p className="uppercase text-[#1a41c0] font-bold text-[1.32rem]">Diseñada para ser veloz</p>
+                <h1 className="font-bold text-[3.6rem] pr-[8rem] leading-[4rem] mb-[1rem] tracking-normal">Actúa rápido e innova aún más rápido.</h1>
+                <p className="text-[1.46rem] whitespace-pre-wrap mr-[6rem]">Llega al mercado antes que nadie usando nuevas funciones a medida que salen y responde a las tendencias de consumo cambiantes en tiempo real, con el apoyo de todo un ecosistema de partners.</p>
             </div>
 
-            <div className="flex w-6/12">
-                <div>
-                    <div className="circle"></div>
-                    <div className="circle"></div>
-                    <div className="circle"></div>
-                    <div className="circle"></div>
-                </div>
-
-                <div>
-                    <p>Migra y pon en marcha tu tienda online en Shopify rápidamente con una plataforma personalizable y fácil de usar.</p>
-                    <p>Lanza rápidamente nuevas campañas, canales, personalizaciones y experimentos y repite cada día.</p>
-                    <p>Automatiza flujos de trabajo repetitivos y de alto rendimiento en todos tus sistemas y tiendas.</p>
-                    <p>Crea cualquier solución que necesite tu negocio mediante un ecosistema de partners y apps especializados.</p>
-                </div>
+            <div className="w-6/12">
+                <ul className="list-image-checkmark text-[1.32rem]">
+                    <li>Migra y pon en marcha tu tienda online en Shopify rápidamente con una plataforma personalizable y fácil de usar.</li>
+                    <li>Lanza rápidamente nuevas campañas, canales, personalizaciones y experimentos y repite cada día.</li>
+                    <li>Automatiza flujos de trabajo repetitivos y de alto rendimiento en todos tus sistemas y tiendas.</li>
+                    <li>Crea cualquier solución que necesite tu negocio mediante un ecosistema de partners y apps especializados.</li>
+                </ul>
             </div>
         </section>
     )
