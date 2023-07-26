cbd76acc82fba79e496dbeb34b5100c6132085c8
Neyan Montes <neyanmontes611@gmail.com>
2023-07-26 01:53:47 -0500

    Proposal 3 Finished

--- src/components/Proposal3.jsx
+++ src/components/Proposal3.jsx
@@ -1,8 +1,11 @@
 export const Proposal3 = () => {
     return (
-        <section className="px-[5.6rem] bg-[#183cbc] text-white">
-            <h1>Shopify Plus es la plataforma de comercio más elegida del mercado. Descubre por qué.</h1>
-            <button>Contacta con el departamento de ventas</button>
+        <section className="px-[5.6rem] bg-[#183cbc] text-white py-[8.2rem]">
+            <h1 className="text-center font-bold text-[3.6rem] leading-[4rem] tracking-normal px-[10rem]">Shopify Plus es la plataforma de comercio más elegida del mercado. Descubre por qué.</h1>
+            
+            <div className="grid grid-cols-1">
+                <button className="mt-[1.5rem] mx-[26rem] bg-[#d8f424] text-black py-[0.8rem] rounded-[50px] text-[1.2rem] font-bold hover:bg-[#b8d40c]">Contactar con el equipo de ventas</button>
+            </div>
         </section>
     )
 }
