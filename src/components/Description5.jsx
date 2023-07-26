dca207e256a434436b45ba18fe019f53148cdaa7
Neyan Montes <neyanmontes611@gmail.com>
2023-07-25 23:22:44 -0500

    Description 5 Finished

--- src/components/Description5.jsx
+++ src/components/Description5.jsx
@@ -1,45 +1,45 @@
 export const Description5 = () => {
     return (
-        <section className="px-[5.6rem] h-screen bg-[#f3f4f6]">
-            <p>Optimizada para el rendimiento</p>
-            <h1>Rinde mejor y olvídate del trabajo pesado</h1>
+        <section className="px-[5.6rem] bg-[#f3f4f6] py-[7.2rem]">
+            <p className="uppercase text-[#1a41c0] font-bold text-[1.32rem]">Optimizada para el rendimiento</p>
+            <h1 className="font-bold text-[3.6rem] pr-[8rem] leading-[4rem] mb-[1rem] tracking-normal pr-[20rem]">Rinde mejor y olvídate del trabajo pesado</h1>
 
-            <div className="grid grid-cols-3">
-                <div>
+            <div className="grid grid-cols-3 gap-[1.5rem] mt-[4rem]">
+                <div className="bg-white p-[2rem] rounded-[15px] border border-black/20 shadow shadow-lg">
                     <img src="https://cdn.shopify.com/b/shopify-brochure2-assets/4d18acece3e77624571d62e4a46a4972.svg?crop=none" alt="img-grid" />
 
-                    <h3>Tiempo promedio de actividad en Shopify: 99,98%</h3>
-                    <a href="">Ver cómo calculamos el tiempo de actividad</a>
+                    <h3 className="my-[1.3rem] font-bold text-[1.7rem] leading-[1.8rem]">Tiempo promedio de actividad en Shopify: 99,98%</h3>
+                    <a className="font-bold text-[1.2rem] leading-[1.8rem] text-[#1a41c0] underline" href="">Ver cómo calculamos el tiempo de actividad</a>
                 </div>
 
-                <div>
+                <div className="bg-white p-[2rem] rounded-[15px] border border-black/20 shadow shadow-lg">
                     <img src="https://cdn.shopify.com/b/shopify-brochure2-assets/05531b19bb846b18c09f979eeb429ad3.svg?crop=none" alt="img-grid" />
 
-                    <h3>Más de 10.000 pagos por minuto con ancho de banda y transacciones ilimitados</h3>
+                    <h3 className="my-[1.3rem] font-bold text-[1.7rem] leading-[1.8rem]">Más de 10.000 pagos por minuto con ancho de banda y transacciones ilimitados</h3>
                 </div>
 
-                <div>
+                <div className="bg-white p-[2rem] rounded-[15px] border border-black/20 shadow shadow-lg">
                     <img src="https://cdn.shopify.com/b/shopify-brochure2-assets/dce7c4174ce9323904a934a486c41288.svg?crop=none" alt="img-grid" />
 
-                    <h3>Seguridad integrada, cumplimiento de la normativa, encriptación de datos y protección contra fraude y bots</h3>
+                    <h3 className="my-[1.3rem] font-bold text-[1.7rem] leading-[1.8rem] whitespace-pre-wrap">Seguridad integrada, cumplimiento de la normativa, encriptación  de datos y protección contra fraude y bots</h3>
                 </div>
 
-                <div>
+                <div className="bg-white p-[2rem] rounded-[15px] border border-black/20 shadow shadow-lg">
                     <img src="https://cdn.shopify.com/b/shopify-brochure2-assets/3aa53cec161c587e51555bdfa5c56eff.svg?crop=none" alt="img-grid" />
 
-                    <h3>Tienda online optimizada para dispositivos móviles con ancho de banda y hosting web ilimitados</h3>
+                    <h3 className="my-[1.3rem] font-bold text-[1.7rem] leading-[1.8rem]">Tienda online optimizada para dispositivos móviles con ancho de banda y hosting web ilimitados</h3>
                 </div>
 
-                <div>
+                <div className="bg-white p-[2rem] rounded-[15px] border border-black/20 shadow shadow-lg">
                     <img src="https://cdn.shopify.com/b/shopify-brochure2-assets/cbd63d84b143ca4d977c56413f0636f8.svg?crop=none" alt="img-grid" />
 
-                    <h3>Datos integrales e información útil con informes detallados en todas tus tiendas</h3>
+                    <h3 className="my-[1.3rem] font-bold text-[1.7rem] leading-[1.8rem]">Datos integrales e información útil con informes detallados en todas tus tiendas</h3>
                 </div>
 
-                <div>
+                <div className="bg-white p-[2rem] rounded-[15px] border border-black/20 shadow shadow-lg">
                     <img src="https://cdn.shopify.com/b/shopify-brochure2-assets/8a5da52ed126447d359e70c05721a8aa.svg?crop=none" alt="img-grid" />
 
-                    <h3>Integraciones, herramientas para desarrolladores y documentación de soporte técnico</h3>
+                    <h3 className="my-[1.3rem] font-bold text-[1.7rem] leading-[1.8rem]">Integraciones, herramientas para desarrolladores y documentación de soporte técnico</h3>
                 </div>
             </div>
         </section>
